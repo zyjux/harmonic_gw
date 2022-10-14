cad65c3a76b8ace6a19e84616d665c9dc86ee67a
Lander Ver Hoef <lander.verhoef@gmail.com>
2022-10-14 13:57:35 -0600

    Added code to use netCDF4 instead of pngs

---
+++ data_preprocess/savNetCDFDNB.py
@@ -0,0 +1,225 @@
+"""
+Gravity Waves Research
+
+Create Images of DNB Data.
+
+By Katherine Haynes
+Edited by Lander Ver Hoef
+
+Modified: 2022/09
+"""
+
+# %%
+# Import libraries
+import numpy as np
+import xarray as xr
+import pandas as pd
+import os
+import functions_map as kfm
+from math import erf, sqrt
+from functions_viirs import read_dnb_sdr, read_GDNBO
+from functions_util import checkNaN, find_index_of_nearest_xy
+from functions_util import scale_eds
+from var_opts import caseOpts
+import var_opts
+# from var_opts import bore20180513_02 as caseDict
+
+caseList = [
+    'bore20180114',
+    'bore20180211_01',
+    'bore20180211_02',
+    'bore20180211_03',
+    'bore20180320_01',
+    'bore20180320_02',
+    'bore20180320_03',
+    'bore20180414_01',
+    'bore20180414_02',
+    'bore20180414_03',
+    'bore20180415_01',
+    'bore20180415_02',
+    # 'bore20180415_03',
+    'bore20180422_01',
+    'bore20180422_02',
+    'bore20180513_01',
+    'bore20180513_02',
+    # 'bore20200424'
+]
+
+# %%
+# User Options
+dirData = 'E:/research_data/2022_harmonic_gravity_waves/'
+nc_savefn = 'E:/research_data/2022_harmonic_gravity_waves/preprocessed_images/bore_ex.nc'
+
+scale_method = 'log'  # 'eds' or 'log' or 'custom'
+
+ds = None
+    
+for case in caseList:
+
+    caseDict = getattr(var_opts, case)
+
+    # %%
+    # Read data and ancillary information
+    data = None
+    lat = None
+    lon = None
+    dataMeta = None
+    fileList = caseDict['fileList']
+    for filename in fileList:
+        fName = dirData + caseDict['filePrefix'] + filename + caseDict['fileSuffix']
+        dataT = read_dnb_sdr(fName, allow_qf=list(range(256)))
+        (lonT, latT, dataMetaT) = read_GDNBO(
+            fName, return_pos=True, return_lunar=True)
+        dataMetaT['datetime'] = [pd.to_datetime(filename[0:17], format="d%Y%m%d_t%H%M%S")]
+            
+        dataT = checkNaN(dataT)
+
+        if data is None:
+            data = dataT
+        else:
+            data = np.concatenate((data, dataT), axis=0)
+
+        if lat is None:
+            lat = latT
+        else:
+            lat = np.concatenate((lat, latT), axis=0)
+
+        if lon is None:
+            lon = lonT
+        else:
+            lon = np.concatenate((lon, lonT), axis=0)
+
+        if dataMeta is None:
+            dataMeta = dataMetaT
+        else:
+            for key in list(dataMeta.keys()):
+                arrOld = dataMeta[key]
+                arrNew = dataMetaT[key]
+                dataMeta[key] = np.concatenate((arrOld, arrNew), axis=0)
+    del dataT
+
+    dataGoodRef = np.where(data >= 0.)
+    dataGoodRef0 = np.where(data > 0.)
+    print("Read data and ancillary information.")
+    print("   Data shape= {}, min= {:.12f}, max= {:.12f}".format(
+        data.shape, data[dataGoodRef].min(), data[dataGoodRef].max()))
+    print("   Lat shape= {}, min= {:.2f}, and max= {:.2f}".format(
+        lat.shape, lat.min(), lat.max()))
+    print("   Lon shape= {}, min= {:.2f}, and max= {:.2f}".format(
+        lon.shape, lon.min(), lon.max()))
+    print("   Number of points non-NaN: {}, >0: {}".format(
+        dataGoodRef[0].shape[0], dataGoodRef0[0].shape[0]))
+
+
+    # %%
+    # Determine contour levels
+    if scale_method == 'eds':
+        solar_zenith_array = dataMeta['solzen']
+        solar_zenith = np.nanmean(solar_zenith_array)
+
+        lunar_zenith_array = dataMeta['lunzen']
+        lunar_zenith = np.nanmean(lunar_zenith_array)
+
+        lunar_fraction_array = dataMeta['lunif']
+        lunar_fraction = np.nanmean(lunar_fraction_array)
+
+        lunar_fraction *= 0.01  # convert from % to decimal
+
+        moon_factor1 = 0.7 * (1.0 - lunar_fraction)
+        moon_factor2 = lunar_zenith * 0.0022
+
+        maxval = 10. ** (-1.7 - (((2.65 + moon_factor1 + moon_factor2))
+                                 * (1 + erf((solar_zenith - 95.)
+                                            / (5. * sqrt(2.0))))))
+        minval = 10. ** (-4. - ((2.95 + moon_factor2)
+                                * (1 + erf((solar_zenith - 95.)
+                                           / (5. * sqrt(2.0))))))
+
+        rangeval = maxval - minval
+        print("Calculated Minval= {:.12f} and Maxval={:.12f}".format(
+            minval, maxval))
+
+        minval1, maxval1 = scale_eds(solar_zenith, lunar_zenith, lunar_fraction)
+        # if minval != minval1 or maxval != maxval1:
+        #    print("Error in scale_eds routine!")
+        #    print("    Scale_eds Minval= {:.12f} and Maxval={:.12f}".
+        #      format(minval1, maxval1))
+        rootbase = 2.
+        radTemp = data.copy()
+
+    elif scale_method == 'log':
+        radLog = data.copy()
+        radLog[dataGoodRef0] = np.log10(radLog[dataGoodRef0])
+        print("Log Min={:.2f} and Max={:.2f}".format(
+            np.min(radLog[dataGoodRef0]), np.max(radLog[dataGoodRef0])))
+        radTemp = radLog
+        del radLog
+
+        minval = -10.25
+        maxval = -8.75
+        rangeval = maxval - minval
+        rootbase = 1.
+
+    elif scale_method == 'custom':
+        radTemp = data.copy()
+        minval = 2.6e-11
+        maxval = 8.6e-10
+        rangeval = maxval - minval
+        rootbase = 3.
+
+    else:
+        print("Invalid scale_method: {}".format(scale_method))
+        print("   Please change to eds, log, or custom.")
+    del data
+
+    # %%
+    # Scale data
+    radTemp = np.divide(np.subtract(radTemp, minval), rangeval, dtype=np.double)
+    radTemp = np.where(radTemp < 0., 0., radTemp)
+    radTemp = radTemp ** (1. / rootbase)  # np.sqrt(radTemp)
+
+    radiance = radTemp
+    del radTemp
+    radMax = np.max(radiance)
+    radMin = np.min(radiance)
+    radScaled = np.where(radiance > 1.0, 1.0, radiance)
+    print("Scaled Radiance Minval= {:.5f} and Maxval={:.5f}".format(
+        radMin, radMax))
+
+
+    # %%
+    # Created scaled data ranging from 0-255
+    rad255 = radScaled.copy() * 255
+    print("0-255 Scaled Radiance Minval= {:.4f} and Maxval= {:.2f}".format(
+        np.nanmin(rad255), np.nanmax(rad255)))
+    
+    
+    # %%
+    # Create DataArray of data and concatenate into dataset
+    case_ds = xr.Dataset(
+        data_vars=dict(
+            rad255=(["x", "y", "time"], np.expand_dims(rad255, axis=-1)),
+            **{
+                key: (["x", "y", "time"], np.expand_dims(dataMeta[key], axis=-1)) for key in [
+                    'senzen',
+                    'senazi',
+                    'senrange',
+                    'solzen',
+                    'solazi',
+                    'lunzen',
+                    'lunazi',
+                ]
+            }
+        ),
+        coords=dict(
+            lon=(["x", "y", "time"], np.expand_dims(lon, axis=-1)),
+            lat=(["x", "y", "time"], np.expand_dims(lat, axis=-1)),
+            time=(["time"], dataMeta['datetime'][0:1])
+        )
+    )
+    if ds is None:
+        ds = case_ds
+    else:
+        ds = xr.concat([ds, case_ds], dim="time")
+
+ds.to_netcdf(nc_savefn)
---
+++ local_autocorr.py
@@ -0,0 +1,75 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from glob import glob
+import re
+
+def auto_correlate(img, x, y, width, height, xrange=None, yrange=None):
+    if xrange==None:
+        xrange = width//2
+    if yrange==None:
+        yrange = height//2
+        
+    img_crop = img[y:y+height, x:x+width]
+    img_crop = (img_crop - img_crop.mean()) / img_crop.std()
+    
+    normalizer = np.sum(img_crop * img_crop)
+    
+    full_xrange = range(0, 2*xrange)
+    full_yrange = range(0, 2*yrange)
+    
+    corr_img = np.zeros((len(full_yrange), len(full_xrange)))
+    
+    for xt in full_xrange:
+        for yt in full_yrange:
+            x_ref = x + (xt - xrange)
+            y_ref = y + (yt - yrange)
+            img_comp = img[y_ref:y_ref+height, x_ref:x_ref+width]
+            img_comp = (img_comp - img_comp.mean()) / img_comp.std()
+            corr_img[yt, xt] = np.sum(img_comp * img_crop) / normalizer
+    
+    corr_img[yrange, xrange] = np.NaN
+            
+    return corr_img
+
+
+def local_auto_corr(img, width, height=None):
+    if height == None:
+        height = width
+    
+    xnum = (img.shape[1] // width) - 1
+    ynum = (img.shape[0] // height) - 1
+    
+    x_vals = [width // 2 + i * width for i in range(xnum)]
+    y_vals = [height // 2 + i * height for i in range(ynum)]
+    
+    corr_im = np.full(img.shape, np.NaN)
+    
+    for x in x_vals:
+        for y in y_vals:
+            corr_im_temp = auto_correlate(img, x, y, width, height)
+            corr_im[y:y+corr_im_temp.shape[0], x:x+corr_im_temp.shape[1]] = corr_im_temp
+            
+    return corr_im
+
+
+img_dir = "E:/research_data/2022_harmonic_gravity_waves/preprocessed_images/"
+fn_list = glob(img_dir + "full_ims/*.png")
+
+square_size = 50
+
+for fn in fn_list:
+    with Image.open(fn).convert('L') as raw_img:
+        img = np.asarray(raw_img, dtype='float64')
+        
+    auto_corr_im = local_auto_corr(img, square_size)
+    
+    sav_fn = img_dir + "auto_corr_ims/" + re.search("d\d.+(?=_log)", fn).group() + "_auto_corr_im.jpg"
+    
+    F = plt.Figure()
+    ax = plt.gca()
+    ax.imshow(auto_corr_im)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    F.set_facecolor("white")
+    plt.savefig(sav_fn, bbox_inches='tight', dpi=400)

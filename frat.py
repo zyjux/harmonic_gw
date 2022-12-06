553a3d14597b462b5f32c5f4f6af16a195cf328c
Lander Ver Hoef <lander.verhoef@gmail.com>
2022-12-06 14:43:08 -0700

    Adjusted signs of x_step and y_step to ensure that we're stepping in an upward direction

--- frat.py
+++ frat.py
@@ -63,8 +63,8 @@ class frt(object):
                 if n_vec[1] == 0:
                     self.r[:, col] = np.sum(self._img, axis=0)
                 else:
-                    x_step = -n_vec[1]
-                    y_step = n_vec[0]
+                    x_step = n_vec[1]
+                    y_step = -n_vec[0]
                     self.r[:, col] += np.roll(self._img[:, int(i * x_step) % self.p], int(i * y_step) % self.p)
 
         return 1/np.sqrt(self.p) * self.r
@@ -95,8 +95,8 @@ class frt(object):
                     if n_vec[1] == 0:
                         self.recon[i, :] += sino[:, col]
                     else:
-                        x_step = -n_vec[1]
-                        y_step = n_vec[0]
+                        x_step = n_vec[1]
+                        y_step = -n_vec[0]
                         if n_vec[0] == 0:
                             self.recon[:, int(i * x_step) % self.p] += np.roll(sino[:, col], int(-i * y_step) % self.p)
                         else:

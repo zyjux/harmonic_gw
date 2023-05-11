1209d70dd5d023f572ccd173e42c306473841b58
Lander Ver Hoef <lander.verhoef@gmail.com>
2023-05-11 13:16:56 -0600

    Added directory creation

--- data_preprocess/conv_nc_to_png.py
+++ data_preprocess/conv_nc_to_png.py
@@ -12,6 +12,9 @@ ds['time'] = pd.to_datetime(ds.time)
 for time in tqdm(ds.time):
     #print(time.values)
     patch = ds.rad255.sel(time=time).values
+
+    (Path.home() / 'research_data/GravityWaves/preprocessed_images/full_ims').mkdir(exist_ok=True)
+
     sav_fn = Path.home() / 'research_data/GravityWaves/preprocessed_images/full_ims' / (re.sub(":", "-", str(time.values)[:-10]) + "_" + str(ds.satellite.sel(time=time).values) + "_log_image.png")
 
     im = Image.fromarray(patch).convert('L')
